fcad70201bd2894d471b0ac98b474f02610daed5
Sehajbir <Sehajbirsingh97@gmail.com>
2021-09-27 11:56:27 +0530

    Added contact form
    Contact Form with validation on email added.
    Submit to work at later stage

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
 import '../styles/Contact.css'
-import {AiOutlineMail} from 'react-icons/ai'
+import {AiOutlineMail, AiOutlineSend} from 'react-icons/ai'
 import { FiInstagram } from 'react-icons/fi'
 import { Element } from 'react-scroll';
 import { BrowserRouter, Link } from 'react-router-dom';
+import {Button} from '../Button'
 
 export class Contact extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            email_error: ''
+        }
+    };
+
+
+    handleEmailChange = event =>{
+        this.setState({ email : event.target.value}, () =>{
+            this.validateEmail();
+        });
+    };
+
+    validateEmail = () =>{
+        const {email} = this.state;
+        this.setState({
+            email_error: 
+                (email.length > 3 && email.indexOf("@") != -1  && email.lastIndexOf(".") != -1 && email.substring(email.lastIndexOf(".")).length > 2) ? null : "Please enter a valid Email"
+        })
+    }
+
     render() {
         return (
             <Element name='contact'>
@@ -40,8 +67,17 @@ export class Contact extends Component {
                             </div>
                         </td>
                         <td className="contact-right">
-
+                            <input type="text" className="contact-form" placeholder="Name" />
+                            <input onChange={this.handleEmailChange} type="text" className="contact-form" placeholder="Email" />
+                            <div className="email-error">
+                                {this.state.email_error}
+                            </div>
+                            <br/>
+                            <textarea placeholder="Message" className="message" name="Message" id="" cols="30" rows="10"></textarea>
+                            <br/>
+                            <Button btnIcon={<AiOutlineSend/>} btnType="btn-outline"></Button>
                         </td>
+                        
                     </tr>
                 </table>
             </Element>
